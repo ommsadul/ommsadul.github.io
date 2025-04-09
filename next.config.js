a84f6478afde0b7a60b6340a13902670959f040d
sadulom13@gmail.com <sadulom13@gmail.com>
2025-04-08 22:10:46 -0400

    feat: update next.config.js and add GitHub Actions workflow for deployment

--- next.config.js
+++ next.config.js
@@ -2,9 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
   images: {
     domains: ['github.com', 'linkedin.com'],
+    unoptimized: true,
   },
+  basePath: '',
   experimental: {
     optimizePackageImports: ['framer-motion'],
     typedRoutes: true
